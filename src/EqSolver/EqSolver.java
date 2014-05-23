77a693b5e76528ebd3a6dfb2228cfaa471259bec
shiayan <toxic.mail@gmail.com>
2014-05-24 03:23:35 +0430

    a5
    a5

--- src/EqSolver/EqSolver.java
+++ src/EqSolver/EqSolver.java
@@ -2,7 +2,7 @@ package EqSolver;
 import java.lang.Math;
 public class EqSolver {
 public double solve(int a,int b,int c){
-	return (b + Math.sqrt(b*b - 4 * a * c))/ 2 * a;
+	return (-b + Math.sqrt(b*b - 4 * a * c))/ 2 * a;
 	
 }
 }
