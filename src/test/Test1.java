f9da6d2b5373160448d94f6a0e876a537e7fcc9e
shiayan <toxic.mail@gmail.com>
2014-05-24 01:33:07 +0430

    a
    a

---
+++ src/test/Test1.java
@@ -0,0 +1,20 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import EqSolver.EqSolver;
+
+public class Test1 {
+
+	@Test
+	public void test() {
+		EqSolver solver = new EqSolver();
+		double result = solver.solve(1,-2,1);
+		//System.out.println(result);
+		assertEquals((int)result, 1,0);
+        
+	}
+
+}
